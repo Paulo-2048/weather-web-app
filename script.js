d4860dc940f1716c766460860178340bcaf3c274
Paulo Vitor <paulo19032004@gmail.com>
2021-03-30 07:49:38 -0300

    Uptade Porject (Add imgs finalize)

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Get a new key api
-let app_id = 'sRRaDRGEykdtS88LghG1cKjZFT96HsEp'
+let app_id = 'rCXJSw3S4rkQsY4VmBIaHQ44pGwYGDjJ'
 let units = 'metrics'
 let search_method = 'zip'
-var lenguage = 'pt-br'
+var lenguage = 'en'
 
 function get_search_method(search_term) {
     if(search_term.length === (5) && Number.parseInt(search_term) + '' === search_term)
@@ -33,11 +32,274 @@ function search_weather(result_from_server) {
 
 function init(weather_finilize) {
     console.log(weather_finilize)
+    if(weather_finilize[0]['IsDayTime']) {
+         switch (weather_finilize[0]['WeatherIcon']) {
+             case 1:
+                // Sunny
+                document.body.style.backgroundImage = 'url("img/sunny.jpg")'
+                break;
+
+            case 2:
+                // Mostly Sunny
+                document.body.style.backgroundImage = 'url("img/mostly-sunny.jpg")'
+                break;
+        
+            case 3:
+                // Partly sunny
+                document.body.style.backgroundImage = 'url("img/partly-sunny.jpg")'
+                break;
+                
+            case 4:
+                // Intermittent Clouds
+                document.body.style.backgroundImage = 'url("img/intermittent-clouds.jpg")'
+                break;
+            
+            case 5:
+                // Hazy Sunshine
+                document.body.style.backgroundImage = 'url("img/hazy-sunshine.jpg")'
+                break;
+
+            case 6:
+                // Mostly Cloudy
+                document.body.style.backgroundImage = 'url("img/mostly-cloudy.jpg")'
+                break;
+        
+            case 13:
+                //Mostly Cloudy w/ Showers
+                document.body.style.backgroundImage = 'url("img/mostly-cloudy-w-showers.jpg")'
+                break;
+                
+            case 14:
+                //Partly Sunny w/ Showers
+                document.body.style.backgroundImage = 'url("img/partly-sunny-w-showers.jpg")'
+                break;
+        
+            case 16:
+                //Mostly Cloudy w/ T-Storms
+                document.body.style.backgroundImage = 'url("img/mostly-cloudy-w-t-Storms.jpg")'
+                break;
+
+            case 17:
+                //Partly Sunny w/ T-Storms
+                document.body.style.backgroundImage = 'url("img/partly-sunny-w-t-Storms.jpg")'
+                break;
+        
+            case 20:
+                //Mostly Cloudy w/ Flurries
+                document.body.style.backgroundImage = 'url("img/mostly-cloudy-w-flurries.jpg")'
+                break;
+                
+            case 21:
+                //Partly Sunny w/ Flurries
+                document.body.style.backgroundImage = 'url("img/partly-sunny-w-flurries.jpg")'
+                break;
+            
+            case 23:
+                //Mostly Cloudy w/ Snow
+                document.body.style.backgroundImage = 'url("img/mostly-cloudy-w-snow")'
+                break;
+        
+            case 7:
+                //Cloudy
+                document.body.style.backgroundImage = 'url("img/cloudy.jpg")'
+                break;
+                
+            case 8:
+                //Dreary (Overcast)
+                document.body.style.backgroundImage = 'url("img/dreary(overcast).jpg")'
+                break;
+            
+            case 11:
+                //Fog
+                document.body.style.backgroundImage = 'url("img/fog.jpg")'
+                break;
+
+            case 12:
+                //Showers
+                document.body.style.backgroundImage = 'url("img/showers.jpg")'
+                break;
+                
+            case 15:
+                //T-Storms
+                document.body.style.backgroundImage = 'url("img/t-storms.jpg")'
+                break;
+            
+            case 18:
+                //Rain
+                document.body.style.backgroundImage = 'url("img/rain.jpg")'
+                break;
+
+            case 19:
+                //Flurries
+                document.body.style.backgroundImage = 'url("img/flurries.jpg")'
+                break;
+                
+            case 22:
+                //Snow
+                document.body.style.backgroundImage = 'url("img/snow.jpg")'
+                break;
+            
+            case (24,25):
+                //Ice
+                document.body.style.backgroundImage = 'url("img/sleet.jpg")'
+                break;
+
+             case 25:
+                //Sleet
+                document.body.style.backgroundImage = 'url("img/sleet.jpg")'
+                break;
+                
+            case 26:
+                //Freezing Rain
+                document.body.style.backgroundImage = 'url("img/freezing-rain.jpg")'
+                break;
+            
+            case 29:
+                //Rain and Snow
+                document.body.style.backgroundImage = 'url("img/rain-and-snow.jpg")'
+                break;
+
+            default:
+                document.body.style.backgroundImage = 'url("img/default.jpg")'
+                break;
+        }
+    }
+    if(!weather_finilize[0]['IsDayTime']) {
+        console.log('Night')
+        switch (weather_finilize[0]['WeatherIcon']) {
+
+            case 7:
+                //Cloudy
+                document.body.style.backgroundImage = 'url("img/cloudy.jpg")'
+                break;
+                
+            case 8:
+                //Dreary (Overcast)
+                document.body.style.backgroundImage = 'url("img/dreary(overcast).jpg")'
+                break;
+            
+            case 11:
+                //Fog
+                document.body.style.backgroundImage = 'url("img/fog.jpg")'
+                break;
+
+            case 12:
+                //Showers
+                document.body.style.backgroundImage = 'url("img/showers.jpg")'
+                break;
+                
+            case 15:
+                //T-Storms
+                document.body.style.backgroundImage = 'url("img/t-storms.jpg")'
+                break;
+            
+            case 18:
+                //Rain
+                document.body.style.backgroundImage = 'url("img/rain.jpg")'
+                break;
+
+            case 19:
+                //Flurries
+                document.body.style.backgroundImage = 'url("img/flurries.jpg")'
+                break;
+                
+            case 22:
+                //Snow
+                document.body.style.backgroundImage = 'url("img/snow.jpg")'
+                break;
+            
+            case (24,25):
+                //Ice
+                document.body.style.backgroundImage = 'url("img/sleet.jpg")'
+                break;
+
+             case 25:
+                //Sleet
+                document.body.style.backgroundImage = 'url("img/sleet.jpg")'
+                break;
+                
+            case 26:
+                //Freezing Rain
+                document.body.style.backgroundImage = 'url("img/freezing-rain.jpg")'
+                break;
+            
+            case 29:
+                //Rain and Snow
+                document.body.style.backgroundImage = 'url("img/rain-and-snow.jpg")'
+                break;
+
+            case 33:
+                //Clear
+                document.body.style.backgroundImage = 'url("img/clear.jpg")'
+                break;
+
+            case 34:
+                // Mostly Clear
+                document.body.style.backgroundImage = 'url("img/mostly-night-clear.jpg")'
+                break;
+
+            case 35:
+                //Partly Cloudy
+                document.body.style.backgroundImage = 'url("img/partly-night-cloud.jpg")'
+                break;
+
+            case 36:
+                //Intermittent Clouds
+                document.body.style.backgroundImage = 'url("img/intermittent-night-clouds.jpg")'
+                break;
+        
+            case 37:
+                //Hazy Moonlight
+                document.body.style.backgroundImage = 'url("img/hazy-moonlight.jpg")'
+                break;
+
+            case 38:
+                //Mostly Cloudy
+                document.body.style.backgroundImage = 'url("img/mostly-night-cloud")'
+                break;
+
+            case 39:
+                //Partly Cloudy w/ Showers
+                document.body.style.backgroundImage = 'url("showers-night.jpg")'
+                break;
+
+            case 40:
+                //Mostly Cloudy w/ Showers
+                document.body.style.backgroundImage = 'url("img/showers-night.jpg")'
+                break;
+
+            case 41:
+                //Partly Cloudy w/ T-Storms
+                document.body.style.backgroundImage = 'url("img/t-storm-night.jpg")'
+                break;
+
+            case 42:
+                //Mostly Cloudy w/ T-Storms
+                document.body.style.backgroundImage = 'url("img/t-storm-night.jpg")'
+                break;
+
+            case 43:
+                //Mostly Cloudy w/ Flurries
+                document.body.style.backgroundImage = 'url("img/flurries-night.jpg")'
+                break;
+
+            case 44:
+                //Mostly Cloudy w/ Snow
+                document.body.style.backgroundImage = 'url("img/snow-night.jpg")'
+                break;
+                
+
+            default:
+                document.body.style.backgroundImage = 'url("img/default.jpg")'
+                break;
+        }
+    }
     
 }
 
 document.getElementById('search_btn').addEventListener('click', () => {
     let search_term = document.getElementById('search_input').value
     if(search_term)
-        search_weather(search_term)
+        search_location(search_term)
+
     })
